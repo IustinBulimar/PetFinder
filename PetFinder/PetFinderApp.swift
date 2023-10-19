f4a2130799cc8c7680e939e5a7b9cf303d487e64
Iustin Bulimar <iustin.bulimar@gmail.com>
2023-10-19 14:10:12 +0300

    Create project

---
+++ PetFinder/PetFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFinderApp.swift
+//  PetFinder
+//
+//  Created by Iustin Bulimar on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PetFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
